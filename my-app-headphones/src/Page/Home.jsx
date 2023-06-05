834f53d9dff51816839e4311476d9d6777f5723a
Fatemeh <fatemehzalaghi@gmail.com>
2023-06-05 21:08:07 +0330

    Adding product photos, app components and reloading

---
+++ my-app-headphones/src/Page/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/Hedear/Header'
+
+function Home() {
+    return (
+        <>
+
+            <Header />
+
+        </>
+    )
+}
+
+export default Home
