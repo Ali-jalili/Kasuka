ab16234c78de300bf7414fe720dcd9d3370a873a
Fatemeh <fatemehzalaghi@gmail.com>
2023-06-23 20:59:31 +0330

    Active Link menu

--- portfolio-website/src/Components/Navbar/Navbar.jsx
+++ portfolio-website/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-import '../../App.css';
 import './Navbar.css';
+import '../../App.css';
 
 
 function Navbar() {
 
+    window.addEventListener('scroll', () => {
+        const sectionNav = document.querySelector('.section-nav');
+
+        if (window.scrollY >= 80) {
+            sectionNav.classList.add('scroll-nav');
+        } else {
+            sectionNav.classList.remove('scroll-nav');
+        }
+    });
+
     const [Toggle, showMenu] = useState(false);
+    const [activNav, setActivNav] = useState('#home');
 
 
 
@@ -25,7 +36,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link active-link' href="#home">
+                                <a href="#home" className={activNav === '#home' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#home')}>
 
                                     <i className='uil uil-estate nav-icon'></i>Home
 
@@ -35,7 +46,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link' href="#about">
+                                <a className={activNav === '#about' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#about')} href="#about">
 
                                     <i className='uil uil-user nav-icon'></i>About
 
@@ -45,7 +56,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link' href="#skills">
+                                <a className={activNav === '#skills' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#skills')} href="#skills">
 
                                     <i className='uil uil-file-alt nav-icon'></i>Skills
 
@@ -55,7 +66,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link' href="#services">
+                                <a className={activNav === '#services' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#services')} href="#services">
 
                                     <i className='uil uil-briefcase-alt nav-icon'></i>Services
 
@@ -65,7 +76,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link' href="#protfolio">
+                                <a className={activNav === '#protfolio' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#protfolio')} href="#protfolio">
 
                                     <i className='uil uil-scenery nav-icon'></i>Protfolio
 
@@ -75,7 +86,7 @@ function Navbar() {
 
                             <li className="nav-item">
 
-                                <a className='nav-link' href="#contact">
+                                <a className={activNav === '#contact' ? 'nav-link active-link' : 'nav-link'} onClick={() => setActivNav('#contact')} href="#contact">
 
                                     <i className='uil uil-message nav-icon'></i>Contact
 
